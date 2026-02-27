daa423b03b4107b6a2d6b8105540ee78555e1727
Benjamin Gobeil <bkgobeil@gmail.com>
2026-02-27 12:44:41 -0500

    feat: add coverage tools

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         include: ['test/**/*.test.ts'],
+        coverage: {
+            provider: 'v8',
+            include: ['src/**/*.ts'],
+            exclude: ['src/generated/**', 'src/infrastructure/abi/**'],
+        },
     },
 });
